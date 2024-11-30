2afbb2700b029a04372d417c54880291467f87f0
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2024-11-30 16:49:05 +0100

    added tmux container logic

--- dot_config/zsh/custom/executable_zoxide.zsh
+++ dot_config/zsh/custom/executable_zoxide.zsh
@@ -1,1 +1,18 @@
+#!/bin/zsh
+
+set -u -e -o pipefail
+
+if [ ! -f "$HOME/.local/bin/zoxide" ]; then
+  echo "Do you want to install zoxide?"
+  read -p -r "Y/n: " response
+  if [ "$response" == "n" ]; then
+    echo "zoxide not installed"
+    return
+  else
+    echo "Installing zoxide"
+    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+  fi
+fi
+
 eval "$(zoxide init zsh)"
+
