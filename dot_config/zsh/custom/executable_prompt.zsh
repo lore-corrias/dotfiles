5b80844befe3b88dd5297933ace9884629d0c1bd
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-01-16 13:40:32 +0100

    changes

---
+++ dot_config/zsh/custom/executable_prompt.zsh
@@ -0,0 +1,33 @@
+# Detect the Linux distribution
+if [[ "$(uname)" == "Linux" ]]; then
+  if [[ -f /etc/os-release ]]; then
+    # Extract the distribution name from /etc/os-release
+    DISTRO=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+
+    case "$DISTRO" in
+      *Ubuntu*)
+        export OS_ICON=""
+        ;;
+      *Alpine*)
+        export OS_ICON=""
+        ;;
+      *Kali*)
+        export OS_ICON=""
+        ;;
+      *Debian*)
+        export OS_ICON=""
+        ;;
+      *Fedora*)
+        export OS_ICON="󰮤"
+        ;;
+      *Arch*)
+        export OS_ICON="󰣇"
+        ;;
+    esac
+  else
+    export OS_ICON="🐧"        # If no specific distro info, default to Linux penguin
+  fi
+fi
+
+# Customize Pure prompt with the OS icon
+export PURE_PROMPT_SYMBOL="%f${OS_ICON}%f  %F{magenta}❯"
