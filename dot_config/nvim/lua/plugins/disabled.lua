e2938e66a9b9dec137a200752f5ad46e379f0592
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-03-09 23:01:14 +0100

    started reworking interface

---
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "folke/trouble.nvim", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "MagicDuck/grug-far.nvim", enabled = false },
+}
---
+++ dot_config/zsh/custom/conda.zsh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+if [ -f /opt/conda/etc/profile.d/conda.sh ]; then
+	source /opt/conda/etc/profile.d/conda.sh
+fi
