0f410e9f8396023bfb8e35fe0b71e0e0e3b1caf7
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-03-31 13:43:58 +0200

    updated nvim

--- dot_config/nvim/lua/conf-plugins/mason.lua
+++ dot_config/nvim/lua/conf-plugins/mason.lua
@@ -2,3 +2,7 @@ require("mason").setup()
 require("mason-lspconfig").setup {
     ensure_installed = { "lua_ls", "dotls" },
 }
+
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").bashls.setup {}
