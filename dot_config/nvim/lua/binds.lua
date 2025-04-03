0280dcf3dad0bb8137c7fe7ecba8c3076581753a
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-04-03 13:19:18 +0200

    nvim - added a couple of key bindings

--- dot_config/nvim/lua/binds.lua
+++ dot_config/nvim/lua/binds.lua
@@ -9,6 +9,11 @@ vim.keymap.set("n", '<leader>v', ':vsplit<CR>', { noremap = true, silent = true 
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
 
+-- Format code
+vim.keymap.set({ 'n', 'v' }, '<leader>f', function()
+  vim.lsp.buf.format { async = true }
+end)
+
 -- LSP bindings for lazy ppl
 -- vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 -- vim.keymap.set("n", "<leader>cr", "<cmd>lua vim.lsp.buf.rename()<CR>")
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -20,8 +20,12 @@ local masonlsp = {
     config = function()
       require("mason").setup()
       require("mason-lspconfig").setup {
-        -- Installing harper, which performs grammar checks
-        ensure_installed = { "harper-ls" },
+        ensure_installed = {
+          -- Installing harper, which performs grammar checks
+          "harper-ls",
+          -- Trivy scans for misconfigurations, exposed secrets, etc.
+          "trivy",
+        },
       }
 
       require("mason-lspconfig").setup_handlers {
@@ -30,16 +34,16 @@ local masonlsp = {
             require("lspconfig")[server_name].setup {}
         end,
         -- Specific handlers
-        -- ["rust_analyzer"] = function ()
-        --     require("rust-tools").setup {}
-        -- end
+        -- https://github.com/mrcjkb/rustaceanvim/blob/master/doc/mason.txt
+        ["rust_analyzer"] = function () end
       }
     end,
     -- Some bindings to navigate through errors more quickly
     keys = {
-      { "<leader>ca", function() vim.lsp.buf.code_action() end, desc = "Open code actions" },
-      { "<leader>cr", function() vim.lsp.buf.rename() end, desc = "Open diagnostics" },
-      { "<leader>cd", function() vim.diagnostic.open_float() end, desc = "Open diagnostics" },
+      { "<leader>a", function() vim.lsp.buf.code_action() end, desc = "Open code actions" },
+      { "<leader>r", function() vim.lsp.buf.rename() end, desc = "Open diagnostics" },
+      { "<leader>d", function() vim.diagnostic.open_float() end, desc = "Open diagnostics" },
+      { "<leader>h", function() vim.diagnostic.hover() end, desc = "Open hover menu" },
     }
   }
 }
@@ -51,6 +55,24 @@ local rust = {
     version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
   }
+}
+
+-- Setting up rustaceanvim to use Mason w/ rust-analyzer
+vim.g.rustaceanvim = {
+  server = {
+    cmd = function()
+      local mason_registry = require('mason-registry')
+      if mason_registry.is_installed('rust-analyzer') then
+        -- This may need to be tweaked depending on the operating system.
+        local ra = mason_registry.get_package('rust-analyzer')
+        local ra_filename = ra:get_receipt():get().links.bin['rust-analyzer']
+        return { ('%s/%s'):format(ra:get_install_path(), ra_filename or 'rust-analyzer') }
+      else
+        -- global installation
+        return { 'rust-analyzer' }
+      end
+    end,
+  },
 }
 
 return {
