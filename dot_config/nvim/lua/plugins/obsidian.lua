b0a192abdfce38ff78d18d9cb0b0251780a324ee
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-04-15 21:50:13 +0200

    [nvim] obsidian changes

--- dot_config/nvim/lua/plugins/obsidian.lua
+++ dot_config/nvim/lua/plugins/obsidian.lua
@@ -3,15 +3,44 @@ vim.opt.conceallevel = 1
 
 return {
   -- Render markdown
-  -- {
-  --   'MeanderingProgrammer/render-markdown.nvim',
-  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you use the mini.nvim suite
-  --   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  --   ---@module 'render-markdown'
-  --   ---@type render.md.UserConfig
-  --   opts = {},
-  -- },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      heading = {
+        -- Turn on / off heading icon & background rendering.
+        enabled = true,
+        -- Turn on / off atx heading rendering.
+        atx = true,
+        -- Turn on / off setext heading rendering.
+        setext = true,
+        -- Turn on / off any sign column related rendering.
+        sign = true,
+        -- Replaces '#+' of 'atx_h._marker'.
+        -- Output is evaluated depending on the type.
+        -- | function | `value(context)`              |
+        -- | string[] | `cycle(value, context.level)` |
+        icons = { ' I ', ' II ', ' III ', ' IV ', ' V ', ' VI ' },
+        -- Determines how icons fill the available space.
+        -- | right   | '#'s are concealed and icon is appended to right side                          |
+        -- | inline  | '#'s are concealed and icon is inlined on left side                            |
+        -- | overlay | icon is left padded with spaces and inserted on left hiding any additional '#' |
+        position = 'overlay',
+        -- Added to the sign column if enabled.
+        -- Output is evaluated by `cycle(value, context.level)`.
+        signs = { '󰫎 ' },
+        -- Width of the heading background.
+        -- | block | width of the heading text |
+        -- | full  | full width of the window  |
+        -- Can also be a list of the above values evaluated by `clamp(value, context.level)`.
+        width = 'block',
+      },
+    },
+  },
   {
     "obsidian-nvim/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
