0e2efbbd13ebc067303fa0c2e3d07838e71a6e8c
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-07-17 18:09:38 +0200

    moved devcontainers management to devcontainers branch

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -2,7 +2,7 @@ return {
   {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets', 'fang2hou/blink-copilot' },
+    dependencies = { 'rafamadriz/friendly-snippets', 'fang2hou/blink-copilot', 'Kaiser-Yang/blink-cmp-avante', },
 
     -- use a release tag to download pre-built binaries
     version = '1.*',
@@ -34,7 +34,7 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+        default = { 'avante', 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
         providers = {
           copilot = {
             name = "copilot",
@@ -42,6 +42,13 @@ return {
             score_offset = 100,
             async = true,
           },
+          avante = {
+            module = 'blink-cmp-avante',
+            name = 'Avante',
+            opts = {
+                -- options for blink-cmp-avante
+            }
+          }
         },
       },
 
--- dot_config/zsh/custom/executable_env.zsh
+++ dot_config/zsh/custom/executable_env.zsh
@@ -2,7 +2,7 @@ export EDITOR="nvim"
 # TODO: Add a script that runs ptyxis through flatpak and reference it here
 export TERMINAL="app.devsuite.Ptyxis"
 
-export PATH=$HOME/.local/bin:$PATH
+export PATH="$PATH:$HOME/.local/bin"
 if [ -d "$HOME/.spicetify" ]; then
-  export PATH=$HOME/.spicetify:$PATH
+  export PATH=$HOME/.poba:$PATH
 fi
