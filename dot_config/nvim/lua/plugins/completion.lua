70362d87ae9409463a71506fc7b825301220ec62
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-07-30 18:15:19 +0200

    added script to create devcontainer file, removed avante and copilot

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -2,7 +2,7 @@ return {
   {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
-    dependencies = { 'rafamadriz/friendly-snippets', 'fang2hou/blink-copilot', 'Kaiser-Yang/blink-cmp-avante', },
+    dependencies = { 'rafamadriz/friendly-snippets' },
 
     -- use a release tag to download pre-built binaries
     version = '1.*',
@@ -34,22 +34,15 @@ return {
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'avante', 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
-        providers = {
-          copilot = {
-            name = "copilot",
-            module = "blink-copilot",
-            score_offset = 100,
-            async = true,
-          },
-          avante = {
-            module = 'blink-cmp-avante',
-            name = 'Avante',
-            opts = {
-                -- options for blink-cmp-avante
-            }
-          }
-        },
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' }
+        -- providers = {
+        --   copilot = {
+        --     name = "copilot",
+        --     module = "blink-copilot",
+        --     score_offset = 100,
+        --     async = true,
+        --   }
+        -- },
       },
 
       -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
