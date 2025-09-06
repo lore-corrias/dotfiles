47126b68c0c37c1eaf43ab761b62364059149ef1
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-09-06 17:00:51 +0200

    various updates

--- dot_config/zsh/custom/executable_env.zsh
+++ dot_config/zsh/custom/executable_env.zsh
@@ -6,3 +6,8 @@ export PATH="$PATH:$HOME/.local/bin"
 if [ -d "$HOME/.spicetify" ]; then
   export PATH=$HOME/.poba:$PATH
 fi
+
+# Locale
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
