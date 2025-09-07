42fa6e3c0cb82886f5f90c6b79a3d552319a7d9c
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-09-07 20:14:28 +0200

    removed obsidian nvim plugin

---
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,14 @@
+-- For obsidian
+vim.opt.conceallevel = 2
+
+-- Render markdown
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  }
+}
+
