03c89c51d0b35fa6d4b0a48384aa21c91f0a9aea
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2025-09-10 12:05:28 +0200

    fixed locales

--- dot_config/zsh/custom/executable_env.zsh
+++ dot_config/zsh/custom/executable_env.zsh
@@ -6,8 +6,3 @@ export PATH="$PATH:$HOME/.local/bin"
 if [ -d "$HOME/.spicetify" ]; then
   export PATH=$HOME/.poba:$PATH
 fi
-
-# Locale
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
