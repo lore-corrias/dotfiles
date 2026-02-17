8659b32a6cd885f65d4721d8c9e3c77f1e2ad5de
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2026-02-17 11:44:52 +0100

    fixed zsh bindings

--- dot_config/zsh/custom/executable_keybindings.zsh
+++ dot_config/zsh/custom/executable_keybindings.zsh
@@ -1,4 +1,17 @@
 # zsh-history-substring-search configuration
 bindkey '^[[A' history-substring-search-up # or '\eOA'
 bindkey '^[[B' history-substring-search-down # or '\eOB'
+
+# Insert mode
+bindkey -M viins '^[[1;5D' backward-word
+bindkey -M viins '^[[1;5C' forward-word
+# Command mode
+bindkey -M vicmd '^[[1;5D' backward-word
+bindkey -M vicmd '^[[1;5C' forward-word
+
+bindkey -M viins "^[[3;5~" kill-word
+bindkey -M vicmd "^[[3;5~" kill-word
+bindkey -M viins '^H' backward-kill-word
+bindkey -M vicmd '^H' backward-kill-word
+
 HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
