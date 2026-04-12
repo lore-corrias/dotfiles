b8b0c482be9e6dc3e2ddc65a2784fdfa25e58374
lore-corrias <32979892+lore-corrias@users.noreply.github.com>
2026-04-12 18:49:04 +0000

    moved neovim from nvim-treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,17 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs",
-    branch = 'master',
-    lazy = false,
-    build = ":TSUpdate",
-    opts = {
-      ensure_installed = { "c", "lua", "vim", "python", "markdown" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = {
-        enable = true,
-      }
-    }
+    "romus204/tree-sitter-manager.nvim",
+    config = function()
+      require("tree-sitter-manager").setup({
+        -- Default Options
+        -- ensure_installed = {}, -- list of parsers to install at the start of a neovim session
+        -- border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
+        auto_install = true, -- if enabled, install missing parsers when editing a new file
+        -- highlight = true, -- treesitter highlighting is enabled by default
+        -- languages = {}, -- override or add new parser sources
+        -- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
+        -- query_dir = vim.fn.stdpath("data") .. "/site/queries",
+      })
+    end
   }
 }
